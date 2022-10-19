9f0bc41873e178e98895fad2a56ce0ec51dc2604
Songnian <1127093059@qq.com>
2022-10-19 16:02:06 +0800

    fix：补充了逻辑判断的条件

--- 手写Object.is/index.js
+++ 手写Object.is/index.js
@@ -4,16 +4,15 @@ NaN === NaN // false
 */
 
 Object.prototype.myIs = function (x, y) {
-    //x和y为数字情况
-    if (x === y) {
-        //x!==0用来处理非0数字，1 / x === 1 / y用于处理0
-        return x !== 0 || 1 / x === 1 / y;
-    }
-    //x和y为NaN情况
-    return x !== x && y !== y;
-}
+	if (x === y) {
+		//运行到1/x === 1/y的时候x和y都为0，但是1/+0 = +Infinity， 1/-0 = -Infinity, 是不一样的
+		return x !== 0 || y !== 0 || 1 / x === 1 / y;
+	}
+	//x和y为NaN情况
+	return x !== x && y !== y;
+};
 
 //test
-console.log('+0,+0',Object.myIs(+0, +0));
-console.log('-0,+0',Object.myIs(-0, +0));
-console.log('0,0',Object.myIs(0, 0));
+console.log("+0,+0", Object.myIs(+0, +0));
+console.log("-0,+0", Object.myIs(-0, +0));
+console.log("0,0", Object.myIs(0, 0));
