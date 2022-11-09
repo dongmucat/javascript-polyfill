30797134753edcf0afdbe776d38f73b766e610f8
Songnian <1127093059@qq.com>
2022-11-09 22:41:05 +0800

    upd：增加测试用例

--- 手写Promise.finally/index.js
+++ 手写Promise.finally/index.js
@@ -1,4 +1,3 @@
-
 /**
  * 参考：https://es6.ruanyifeng.com/#docs/promise#Promise-prototype-finally
  * finally会返回一个Promise本质上是then方法，它接收一个callback，无论成功还是失败都会去执行它
@@ -7,6 +6,16 @@
 Promise.prototype.myFinally = function (callback) {
 	return this.then(
 		(value) => Promise.resolve(callback()).then(() => value),
-		(reason) => Promise.resolve(callback()).then(() => {throw reason})
+		(reason) =>
+			Promise.resolve(callback()).then(() => {
+				throw reason;
+			})
 	);
 };
+
+const p = new Promise((resolve, reject) => {
+	resolve("成功了");
+})
+	.then((data) => data)
+	.myFinally(() => console.log("this is myFinally"))
+	.then((data) => console.log(data));
